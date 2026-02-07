6a0cf25d1c952fa37baea959fd4f2ab9a432e49d
Piotr Doan <piotr.doan@proton.me>
2026-02-07 16:00:28 +0100

    Disable logging in release

--- include/dn/shared.h
+++ include/dn/shared.h
@@ -8,9 +8,22 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <dxgi1_6.h>
-#include <dxgidebug.h>
 #include <d3d11_4.h>
 #include <d3dcompiler.h>
+#include <dxgidebug.h>
+
+#ifndef DN_CONFIG_RELEASE
+  #define DN_LOG_INFO(format, ...) \
+    fprintf(stdout, format "\n" __VA_OPT__(,) __VA_ARGS__);
+
+  #define DN_LOG_ERROR(format, ...) \
+    fprintf(stderr, format "\n" __VA_OPT__(,) __VA_ARGS__); \
+    fflush(stdout); \
+    fflush(stderr)
+#else
+  #define DN_LOG_INFO(format, ...)
+  #define DN_LOG_ERROR(format, ...)
+#endif
 
 #define DN_UNUSED(x) (void)(x)
 #define DN_ARRAY_LENGTH(array) (sizeof(array) / sizeof((array)[0]))
@@ -32,11 +45,3 @@
     auto _b = (b); \
     _a < _b ? _a : _b; \
   })
-
-#define DN_LOG_INFO(format, ...) \
-  fprintf(stdout, format "\n" __VA_OPT__(,) __VA_ARGS__);
-
-#define DN_LOG_ERROR(format, ...) \
-  fprintf(stderr, format "\n" __VA_OPT__(,) __VA_ARGS__); \
-  fflush(stdout); \
-  fflush(stderr)

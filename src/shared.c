f9498966027957cd9eef2766aa7d9652711b9dfe
Piotr Doan <piotr.doan@proton.me>
2026-03-06 02:17:44 +0100

    Flush stdout before stderr on error

--- src/shared.c
+++ src/shared.c
@@ -15,7 +15,7 @@ void DnLog_Error(const char* format, ...)
   va_list args;
   va_start(args, format);
   vfprintf(stderr, format, args);
-  fflush(stderr);
   fflush(stdout);
+  fflush(stderr);
   va_end(args);
 }

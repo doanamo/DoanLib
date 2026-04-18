e2126885c8e63f302730a429a35a297e75016a38
Piotr Doan <doanpiotr@gmail.com>
2026-04-18 15:45:53 +0200

    Fix window size

--- example/main.c
+++ example/main.c
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]) {
     .system = {
       .windowTitle = "DoanLib Example",
       .windowWidth = 1024,
-      .windowHeight = 768,
+      .windowHeight = 576,
     }
   };
 

338e84da8c496a77507e916a126bf6eda6111438
Piotr Doan <doanpiotr@gmail.com>
2026-05-24 16:36:35 +0200

    Physical graphics device selection

--- src/gpu/context.c
+++ src/gpu/context.c
@@ -4,57 +4,21 @@
 struct DnGpuContext {
   VkInstance instance;
   VkDebugUtilsMessengerEXT debugMessenger;
+  VkPhysicalDevice physicalDevice;
 };
 
-#ifdef DN_CONFIG_DEBUG
-
-static VKAPI_ATTR VkBool32 VKAPI_CALL DnGpuContext_DebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT severity, VkDebugUtilsMessageTypeFlagsEXT type, const VkDebugUtilsMessengerCallbackDataEXT* callbackData, void* userData) {
-  DN_UNUSED(severity);
-  DN_UNUSED(userData);
-
-  const char* typeText;
-  switch (type) {
-    case VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT:
-      typeText = "General";
-      break;
-
-    case VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT:
-      typeText = "Validation";
-      break;
-
-    case VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT:
-      typeText = "Performance";
-      break;
-
-    default:
-      typeText = "Unknown";
-      break;
-  }
-
-  if (severity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT) {
-    DN_LOG_ERROR("Vulkan: %s: %s", typeText, callbackData->pMessage);
-  }
-  else {
-    DN_LOG_INFO("Vulkan: %s: %s", typeText, callbackData->pMessage);
-  }
-
-  return VK_FALSE;
-}
-
-#endif
-
 static void DnGpuContext_PrintAvailableInstanceLayers() {
-  bool success = false;
-
   DnMemTempScope tempScope = DnMemTemp_PushScope();
 
   u32 availableLayerCount = 0;
   if (vkEnumerateInstanceLayerProperties(&availableLayerCount, nullptr) != VK_SUCCESS) {
+    DN_LOG_ERROR("Failed to enumerate available instance layer count");
     goto error;
   }
 
   VkLayerProperties* availableLayers = DN_MEM_ALLOC_TYPES(g_dnMemAllocatorTemp, VkLayerProperties, availableLayerCount);
   if (vkEnumerateInstanceLayerProperties(&availableLayerCount, availableLayers) != VK_SUCCESS) {
+    DN_LOG_ERROR("Failed to enumerate available instance layers");
     goto error;
   }
 
@@ -63,28 +27,22 @@ static void DnGpuContext_PrintAvailableInstanceLayers() {
     DN_LOG_INFO("  %s", availableLayers[i].layerName);
   }
 
-  success = true;
-
 error:
-  if (!success) {
-    DN_LOG_ERROR("Failed to enumerate available instance layers");
-  }
-
   DnMemTemp_PopScope(&tempScope);
 }
 
 static void DnGpuContext_PrintAvailableInstanceExtensions() {
-  bool success = false;
-
   DnMemTempScope tempScope = DnMemTemp_PushScope();
 
   u32 availableExtensionCount = 0;
   if (vkEnumerateInstanceExtensionProperties(nullptr, &availableExtensionCount, nullptr) != VK_SUCCESS) {
+    DN_LOG_ERROR("Failed to enumerate available instance extension count");
     goto error;
   }
 
   VkExtensionProperties* availableExtensions = DN_MEM_ALLOC_TYPES(g_dnMemAllocatorTemp, VkExtensionProperties, availableExtensionCount);
   if (vkEnumerateInstanceExtensionProperties(nullptr, &availableExtensionCount, availableExtensions) != VK_SUCCESS) {
+    DN_LOG_ERROR("Failed to enumerate available instance extensions");
     goto error;
   }
 
@@ -93,27 +51,12 @@ static void DnGpuContext_PrintAvailableInstanceExtensions() {
     DN_LOG_INFO("  %s", availableExtensions[i].extensionName);
   }
 
-  success = true;
-
 error:
-  if (!success) {
-    DN_LOG_ERROR("Failed to enumerate available instance extensions");
-  }
-
   DnMemTemp_PopScope(&tempScope);
 }
 
-DnGpuContext* DnGpuContext_Create() {
-  DN_LOG_INFO("Creating gpu context");
+static bool DnGpuContext_CreateInstance(DnGpuContext* context) {
   bool success = false;
-
-  DnGpuContext* context = DN_MEM_ALLOC_TYPE(g_dnMemAllocatorDefault, DnGpuContext);
-  *context = (DnGpuContext) {};
-
-  if (volkInitialize() != VK_SUCCESS) {
-    DN_LOG_ERROR("Failed to initialize volk");
-    goto error;
-  }
 
   VkApplicationInfo appInfo = {
     .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
@@ -158,7 +101,50 @@ DnGpuContext* DnGpuContext_Create() {
 
   volkLoadInstance(context->instance);
 
+  success = true;
+
+error:
+  return success;
+}
+
 #ifdef DN_CONFIG_DEBUG
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL DnGpuContext_DebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT severity, VkDebugUtilsMessageTypeFlagsEXT type, const VkDebugUtilsMessengerCallbackDataEXT* callbackData, void* userData) {
+  DN_UNUSED(severity);
+  DN_UNUSED(userData);
+
+  const char* typeText;
+  switch (type) {
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT:
+      typeText = "General";
+      break;
+
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT:
+      typeText = "Validation";
+      break;
+
+    case VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT:
+      typeText = "Performance";
+      break;
+
+    default:
+      typeText = "Unknown";
+      break;
+  }
+
+  if (severity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT) {
+    DN_LOG_ERROR("Vulkan: %s: %s", typeText, callbackData->pMessage);
+  }
+  else {
+    DN_LOG_INFO("Vulkan: %s: %s", typeText, callbackData->pMessage);
+  }
+
+  return VK_FALSE;
+}
+
+static bool DnGpuContext_CreateDebugMessenger(DnGpuContext* context) {
+  bool success = false;
+
   VkDebugUtilsMessengerCreateInfoEXT debugMessangerCreateInfo = {
     .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
     .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
@@ -170,7 +156,97 @@ DnGpuContext* DnGpuContext_Create() {
     DN_LOG_ERROR("Failed to create debug  messenger");
     goto error;
   }
+
+  success = true;
+
+error:
+  return success;
+}
+
 #endif
+
+static bool DnGpuContext_CreatePhysicalDevice(DnGpuContext* context) {
+  bool success = false;
+
+  DnMemTempScope tempScope = DnMemTemp_PushScope();
+
+  u32 physicalDeviceCount;
+  if (vkEnumeratePhysicalDevices(context->instance, &physicalDeviceCount, nullptr) != VK_SUCCESS) {
+    DN_LOG_ERROR("Failed to enumerate physical graphics device count");
+    goto error;
+  }
+
+  if (physicalDeviceCount == 0) {
+    DN_LOG_ERROR("No physical graphics graphics devices found");
+    goto error;
+  }
+
+  VkPhysicalDevice* physicalDevices = DN_MEM_ALLOC_TYPES(g_dnMemAllocatorTemp, VkPhysicalDevice, physicalDeviceCount);
+  if (vkEnumeratePhysicalDevices(context->instance, &physicalDeviceCount, physicalDevices) != VK_SUCCESS) {
+    DN_LOG_ERROR("Failed to enumerate physical graphics devices");
+    goto error;
+  }
+
+  DN_LOG_INFO("Available physical graphics devices:");
+  for (u32 i = 0; i < physicalDeviceCount; i++) {
+    VkPhysicalDevice physicalDevice = physicalDevices[i];
+    VkPhysicalDeviceProperties physicalDeviceProperties;
+    vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
+    DN_LOG_INFO("  %s", physicalDeviceProperties.deviceName);
+  }
+
+  for (u32 i = 0; i < physicalDeviceCount; i++) {
+    VkPhysicalDevice physicalDevice = physicalDevices[i];
+    VkPhysicalDeviceProperties physicalDeviceProperties;
+    vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
+
+    if (physicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+      context->physicalDevice = physicalDevice;
+      break;
+    }
+  }
+
+  if (context->physicalDevice == nullptr) {
+    context->physicalDevice = physicalDevices[0];
+  }
+
+  VkPhysicalDeviceProperties physicalDeviceProperties;
+  vkGetPhysicalDeviceProperties(context->physicalDevice, &physicalDeviceProperties);
+  DN_LOG_INFO("Physical graphics device: %s", physicalDeviceProperties.deviceName);
+
+  success = true;
+
+error:
+  DnMemTemp_PopScope(&tempScope);
+
+  return success;
+}
+
+DnGpuContext* DnGpuContext_Create() {
+  DN_LOG_INFO("Creating gpu context");
+  bool success = false;
+
+  DnGpuContext* context = DN_MEM_ALLOC_TYPE(g_dnMemAllocatorDefault, DnGpuContext);
+  *context = (DnGpuContext) {};
+
+  if (volkInitialize() != VK_SUCCESS) {
+    DN_LOG_ERROR("Failed to initialize volk");
+    goto error;
+  }
+
+  if (!DnGpuContext_CreateInstance(context)) {
+    goto error;
+  }
+
+#ifdef DN_CONFIG_DEBUG
+  if (!DnGpuContext_CreateDebugMessenger(context)) {
+    goto error;
+  }
+#endif
+
+  if (!DnGpuContext_CreatePhysicalDevice(context)) {
+    goto error;
+  }
 
   success = true;
 

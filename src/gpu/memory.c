ae8752cc4873a1c0e905ad8c5f758b8e61f9ad2c
Piotr Doan <doanpiotr@gmail.com>
2026-05-25 01:27:28 +0200

    Remove VMA dependency

--- src/gpu/memory.c
+++ src/gpu/memory.c
@@ -1,26 +1,21 @@
 #include "dn/gpu.h"
 #include "dn/memory.h"
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wnullability-completeness"
-#include <vma/vk_mem_alloc.h>
-#pragma clang diagnostic pop
-
-static void* VKAPI_PTR DnGpuAllocatorVulkan_Alloc(void* userData, u64 size, u64 alignment, VkSystemAllocationScope scope) {
+static void* VKAPI_PTR DnGpuMemoryVulkan_Alloc(void* userData, u64 size, u64 alignment, VkSystemAllocationScope scope) {
   DN_UNUSED(userData);
   DN_UNUSED(scope);
 
   return DN_MEM_ALLOC_ALIGNED(g_dnMemAllocatorDefault, size, alignment);
 }
 
-static void* VKAPI_PTR DnGpuAllocatorVulkan_Realloc(void* userData, void* allocation, u64 size, u64 alignment, VkSystemAllocationScope scope) {
+static void* VKAPI_PTR DnGpuMemoryVulkan_Realloc(void* userData, void* allocation, u64 size, u64 alignment, VkSystemAllocationScope scope) {
   DN_UNUSED(userData);
   DN_UNUSED(scope);
 
   return DN_MEM_REALLOC_ALIGNED(g_dnMemAllocatorDefault, allocation, size, alignment);
 }
 
-static void VKAPI_PTR DnGpuAllocatorVulkan_Free(void* userData, void* allocation) {
+static void VKAPI_PTR DnGpuMemoryVulkan_Free(void* userData, void* allocation) {
   DN_UNUSED(userData);
 
   DN_MEM_FREE_ALIGNED(g_dnMemAllocatorDefault, allocation);
@@ -28,9 +23,9 @@ static void VKAPI_PTR DnGpuAllocatorVulkan_Free(void* userData, void* allocation
 
 static VkAllocationCallbacks g_dnGpuAllocatorVulkanPrivate = {
   .pUserData = nullptr,
-  .pfnAllocation = DnGpuAllocatorVulkan_Alloc,
-  .pfnReallocation = DnGpuAllocatorVulkan_Realloc,
-  .pfnFree = DnGpuAllocatorVulkan_Free,
+  .pfnAllocation = DnGpuMemoryVulkan_Alloc,
+  .pfnReallocation = DnGpuMemoryVulkan_Realloc,
+  .pfnFree = DnGpuMemoryVulkan_Free,
   .pfnInternalAllocation = nullptr,
   .pfnInternalFree = nullptr,
 };

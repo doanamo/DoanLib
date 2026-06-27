2d7536d3e2376df704495bef8774756fb7661c26
Piotr Doan <doanpiotr@gmail.com>
2026-06-28 01:38:27 +0200

    Fundamental type size assert

--- src/shared.c
+++ src/shared.c
@@ -1,5 +1,16 @@
 #include "dn/shared.h"
 
+static_assert(sizeof(i8) == 1);
+static_assert(sizeof(i16) == 2);
+static_assert(sizeof(i32) == 4);
+static_assert(sizeof(i64) == 8);
+static_assert(sizeof(u8) == 1);
+static_assert(sizeof(u16) == 2);
+static_assert(sizeof(u32) == 4);
+static_assert(sizeof(u64) == 8);
+static_assert(sizeof(f32) == 4);
+static_assert(sizeof(f64) == 8);
+
 #if DN_LOG_ENABLED
 
 void DnLog_Info(const char* format, ...) {
